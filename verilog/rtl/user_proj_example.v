5f09d486fb40d6f6184df8f321fb527d26a49eab
Akshay1000101 <akshayar800@gmail.com>
2023-11-24 22:08:32 +0530

    commiting by akshay

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -45,7 +45,7 @@ module user_proj_example #(
 
     // Wishbone Slave ports (WB MI A)
     input wb_clk_i,
-    input wb_rst_i,
+   /* input wb_rst_i,
     input wbs_stb_i,
     input wbs_cyc_i,
     input wbs_we_i,
@@ -53,104 +53,54 @@ module user_proj_example #(
     input [31:0] wbs_dat_i,
     input [31:0] wbs_adr_i,
     output wbs_ack_o,
-    output [31:0] wbs_dat_o,
+    output [31:0] wbs_dat_o,*/
 
     // Logic Analyzer Signals
-    input  [127:0] la_data_in,
+    /*input  [127:0] la_data_in,
     output [127:0] la_data_out,
-    input  [127:0] la_oenb,
+    input  [127:0] la_oenb,*/
 
     // IOs
-    input  [BITS-1:0] io_in,
-    output [BITS-1:0] io_out,
-    output [BITS-1:0] io_oeb,
+    input   [1:0]io_in,
+    output [3:0] io_out,
+    //output [BITS-1:0] io_oeb,
 
     // IRQ
-    output [2:0] irq
+    //output [2:0] irq
 );
-    wire clk;
-    wire rst;
+    
+   
 
-    wire [BITS-1:0] rdata; 
-    wire [BITS-1:0] wdata;
-    wire [BITS-1:0] count;
 
-    wire valid;
-    wire [3:0] wstrb;
-    wire [BITS-1:0] la_write;
 
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = {{(32-BITS){1'b0}}, rdata};
-    assign wdata = wbs_dat_i[BITS-1:0];
-
-    // IO
-    assign io_out = count;
-    assign io_oeb = {(BITS){rst}};
-
-    // IRQ
-    assign irq = 3'b000;	// Unused
-
-    // LA
-    assign la_data_out = {{(128-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:64-BITS] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
-
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i[BITS-1:0]),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:64-BITS]),
-        .count(count)
-    );
+    pes_sipo dut(.clk(wb_clk_i),.b(io_in[0]),.q(io_out[3:0]));
 
 endmodule
+`timescale 1ns / 1ps
 
-module counter #(
-    parameter BITS = 16
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output reg ready,
-    output reg [BITS-1:0] rdata,
-    output reg [BITS-1:0] count
-);
+module pes_sipo(input clk,b,output[3:0]q);
 
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 1'b0;
-            ready <= 1'b0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1'b1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
-    end
+d_ff dut1(.clk(clk),.d(b),.q(q[3]),.rst());
+d_ff dut2(.clk(clk),.d(q[3]),.q(q[2]),.rst());
+d_ff dut3(.clk(clk),.d(q[2]),.q(q[1]),.rst());
+d_ff dut4(.clk(clk),.d(q[1]),.q(q[0]),.rst());
+
+endmodule
+// d flip flop
+
+module d_ff (
+  input clk,    
+  input d,      
+  input rst,    
+  output reg q);
+
+  always @(posedge clk) 
+  begin
+    if (rst)
+      q <= 1'b0;
+    else
+      q <= d;
+  end
 
 endmodule
 `default_nettype wire
